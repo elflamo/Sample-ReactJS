e0e9338f0bf9f172299b37cfc9979a5b59d65343
Shikhar <shikhar@bigdrop.io>
2018-02-19 16:36:26 +0530

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,1 +1,34 @@
 import React from 'react';
+
+class SideBar extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+
+    logout = event => {
+        localStorage.clear();
+        window.location.href = "/";
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="sidebar">
+                    <div>
+                        <ul>
+                            <li><a>Home</a></li>
+                            <li><a href="#stores" data-toggle="collapse">Stores</a></li>
+                            <li><a href="#offers" data-toggle="collapse">Offers</a></li>
+                            <li><a href="#views" data-toggle="collapse">Views</a></li>
+                            <li><a onClick={this.logout}>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SideBar;
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {dashboardBaseApi} from '../api';
+import SideBar from './Sidebar';
 
 
 class Dashboard extends React.Component{
@@ -12,15 +13,31 @@ class Dashboard extends React.Component{
         let basics = dashboardBaseApi();
     }
 
+    constructor(props) {
+        super(props);
+        // this.toggleSideBar = this.toggleSideBar.bind(this);
+    }
+
+
+    // toggleSideBar = event => {
+    //     this.setState({
+    //         sidebar:!this.state.sidebar,
+    //         open:!this.state.open
+    //     });
+    // };
+
 
     render() {
 
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="stores">
-                            <h3>Stores</h3>
+            <div>
+                <SideBar />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="stores">
+                                <h3>My StoreFront</h3>
+                            </div>
                         </div>
                     </div>
                 </div>
